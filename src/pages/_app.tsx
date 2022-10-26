e3cf6da5b3ceeb40b14676150ff9f80d94a6a2d6
Juan Rodriguez <juanpa.box.77@gmail.com>
2022-10-26 16:09:41 -0300

    :sparkles: feat(ui): add Layout and navBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store, { wrapper } from '../store'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
