1ddb0ba2b80559418218e12432c87f2de89c730c
Juan Rodriguez <juanpa.box.77@gmail.com>
2022-11-07 12:16:16 -0300

    :sparkles: feat(custom): add filter toggle in show transaction page

--- src/components/transactionList/useTransactionList.tsx
+++ src/components/transactionList/useTransactionList.tsx
@@ -2,19 +2,32 @@
 import { startOfWeek } from "date-fns"
 import { useEffect, useState } from "react"
 import { Transaction } from "../../global"
-import { getTransactions } from "../../services/pouchDb"
+import { getTransactions, Transactions } from "../../services/pouchDb"
+import { sharingFilter } from "../../services/sharing-filter"
 import { formatNumberMonth } from "../../utility/formatData"
 
 const useTransactionList = () => {
   const currentMonth = formatNumberMonth(startOfWeek(new Date(), { weekStartsOn: 1 }).getMonth())
 
   const [load, setLoad] = useState(true)
+  const [allTransactions, setAllTransactions] = useState<Transactions>()
   const [transactionsList, setTransactionsList] = useState<Transaction[]>([])
+  const subscriptionToggle = sharingFilter.getSubject
+
+  useEffect(() => {
+    let isActive = true
+    subscriptionToggle.subscribe(filters => {
+      allTransactions && setTransactionsList(allTransactions[filters.type])
+    })
+
+    setLoad(false)
+    return () => { isActive = false }
+  }, [allTransactions])
 
   useEffect(() => {
     let isActive = true
     getTransactions(currentMonth)
-      .then(transactions => setTransactionsList(transactions.expenses))
+      .then(transactions => setAllTransactions(transactions))
 
     setLoad(false)
     return () => {
--- src/pages/showTransactions/index.tsx
+++ src/pages/showTransactions/index.tsx
@@ -1,10 +1,20 @@
 
+import { es } from 'date-fns/locale'
+import { useRouter } from 'next/router'
 import React from 'react'
+import Toggle from '../../components/btn/Toggle'
 import TransactionList from '../../components/transactionList'
+import en from '../../public/locale/en'
 
 const ShowTransactions = () => {
+  const router = useRouter()
+  const { locale } = router
+  const t = locale === 'en' ? en : es
   return (
-    <TransactionList />
+    <>
+      <Toggle locale={locale} />
+      <TransactionList />
+    </>
   )
 }
 
