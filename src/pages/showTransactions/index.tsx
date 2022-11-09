67100f87ca64cf07a45301035b10d65f87f8cb8f
Juan Rodriguez <juanpa.box.77@gmail.com>
2022-11-09 10:03:21 -0300

    :bug: fix(ui): fix bug scrroll in WrapperSelectedFilter

--- src/pages/showTransactions/index.tsx
+++ src/pages/showTransactions/index.tsx
@@ -6,17 +6,18 @@ import Toggle from '../../components/btn/Toggle'
 import Filters from '../../components/filters'
 import TransactionList from '../../components/transactionList'
 import en from '../../public/locale/en'
+import { Wrapper } from './styled'
 
 const ShowTransactions = () => {
   const router = useRouter()
   const { locale } = router
   const t = locale === 'en' ? en : es
   return (
-    <>
+    <Wrapper>
       <Filters locale={locale} />
       <Toggle locale={locale} />
       <TransactionList />
-    </>
+    </Wrapper>
   )
 }
 
