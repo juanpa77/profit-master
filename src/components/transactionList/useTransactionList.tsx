73c1777401af768631a07e2a2145f86292e16d04
Juan Rodriguez <juanpa.box.77@gmail.com>
2022-11-10 13:08:47 -0300

    :construction: wip(custom): add subcription to filters month, change naming

--- src/components/transactionList/useTransactionList.tsx
+++ src/components/transactionList/useTransactionList.tsx
@@ -1,7 +1,8 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+// /* eslint-disable react-hooks/exhaustive-deps */
 import { startOfWeek } from "date-fns"
 import { useEffect, useState } from "react"
 import { Filters, Transaction } from "../../global"
+import { filterTransactionPerWeek } from "../../services/filterTransactions/byDate"
 import { getTransactions, Transactions } from "../../services/pouchDb"
 import { sharingFilter } from "../../services/sharing-filter"
 import { formatNumberMonth } from "../../utility/formatData"
@@ -10,50 +11,68 @@ const useTransactionList = () => {
   const currentMonth = formatNumberMonth(startOfWeek(new Date(), { weekStartsOn: 1 }).getMonth())
 
   const [load, setLoad] = useState(true)
-  const [allTransactions, setAllTransactions] = useState<Transactions>()
+  const [allTransactions, setAllTransactions] = useState<Transactions>({ expenses: [], income: [] })
   const [transactionsList, setTransactionsList] = useState<Transaction[]>([])
 
   const [filters, setFilters] = useState<Filters>()
-  const subscriptionToggle = sharingFilter.getSubject
+  const subscriptionFilters = sharingFilter.getSubject
 
+  // ==> Get all Transactions when component is mounted
   useEffect(() => {
     let isActive = true
-    subscriptionToggle.subscribe(filters => {
-      allTransactions && setTransactionsList(allTransactions[filters.type])
-    })
-
+    if (isActive) {
+      getTransactions(currentMonth)
+        .then(transactions => setAllTransactions(transactions))
+    }
     setLoad(false)
-    return () => { isActive = false }
-  }, [allTransactions])
-
-  useEffect(() => {
-    sharingFilter.getSubject.subscribe({
-      next: (filters) => setFilters(filters)
-    })
-  })
-
-  useEffect(() => {
-    let isActive = true
-    subscriptionToggle.subscribe(filters => {
-      getTransactions(filters.month)
-        .then(transactions => setTransactionsList(transactions[filters.type]))
-    })
-
-    setLoad(false)
-    return () => { isActive = false }
-  }, [filters?.month])
-
-  useEffect(() => {
-    let isActive = true
-    getTransactions(currentMonth)
-      .then(transactions => setAllTransactions(transactions))
-
-    setLoad(false)
+    // console.log('Get all Transactions when component is mounted')
     return () => {
       isActive = false
     }
   }, [])
 
+  // ==> get filters 
+  useEffect(() => {
+    sharingFilter.getSubject.subscribe({
+      next: (filters) => setFilters(filters)
+    })
+    // console.log(filters)
+  })
+
+  //  ==> Filter for week
+  useEffect(() => {
+    subscriptionFilters.subscribe(filters => {
+      setTransactionsList(
+        filterTransactionPerWeek(allTransactions[filters.type], parseInt(filters.week))
+      )
+    })
+    // console.log(filterTransactionPerWeek(allTransactions[filters.type], parseInt(filters.week)))
+  }, [filters?.week])
+
+  // ==> Filter for Type when change allTransactions and type filter
+  useEffect(() => {
+    let isActive = true
+    if (isActive) {
+      subscriptionFilters.subscribe(filters => {
+        allTransactions && setTransactionsList(allTransactions[filters.type])
+      })
+    }
+    setLoad(false)
+    return () => { isActive = false }
+  }, [filters, allTransactions, subscriptionFilters])
+
+  // ==> get Transactions DB when change month filter And setAllTransactions
+  useEffect(() => {
+    let isActive = true
+    if (isActive) {
+      subscriptionFilters.subscribe(filters => {
+        getTransactions(filters.month)
+          .then(transactions => setAllTransactions(transactions))
+      })
+    }
+    setLoad(false)
+    return () => { isActive = false }
+  }, [filters?.month])
 
   return transactionsList
 }
