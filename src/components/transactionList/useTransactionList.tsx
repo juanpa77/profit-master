973005708a27cd73ad0e38a73db99a72fbd91ac2
Juan Rodriguez <juanpa.box.77@gmail.com>
2022-11-25 10:44:04 -0300

    :lipstick: style(ui): add selectedFilter component to display the filter options when user clicks

--- src/components/transactionList/useTransactionList.tsx
+++ src/components/transactionList/useTransactionList.tsx
@@ -1,78 +1,14 @@
-// /* eslint-disable react-hooks/exhaustive-deps */
-import { startOfWeek } from "date-fns"
 import { useEffect, useState } from "react"
-import { Filters, Transaction } from "../../global"
-import { filterTransactionPerWeek } from "../../services/filterTransactions/byDate"
-import { getTransactions, Transactions } from "../../services/pouchDb"
-import { sharingFilter } from "../../services/sharing-filter"
-import { formatNumberMonth } from "../../utility/formatData"
+import { Transaction } from "../../global"
+import { useAppSelector } from "../../redux/useApp"
 
 const useTransactionList = () => {
-  const currentMonth = formatNumberMonth(startOfWeek(new Date(), { weekStartsOn: 1 }).getMonth())
-
-  const [load, setLoad] = useState(true)
-  const [allTransactions, setAllTransactions] = useState<Transactions>({ expenses: [], income: [] })
   const [transactionsList, setTransactionsList] = useState<Transaction[]>([])
+  const transactionSelector = useAppSelector(state => state.transactions)
 
-  const [filters, setFilters] = useState<Filters>()
-  const subscriptionFilters = sharingFilter.getSubject
-
-  // ==> Get all Transactions when component is mounted
   useEffect(() => {
-    let isActive = true
-    if (isActive) {
-      getTransactions(currentMonth)
-        .then(transactions => setAllTransactions(transactions))
-    }
-    setLoad(false)
-    // console.log('Get all Transactions when component is mounted')
-    return () => {
-      isActive = false
-    }
-  }, [])
-
-  // ==> get filters 
-  useEffect(() => {
-    sharingFilter.getSubject.subscribe({
-      next: (filters) => setFilters(filters)
-    })
-    // console.log(filters)
-  })
-
-  //  ==> Filter for week
-  useEffect(() => {
-    subscriptionFilters.subscribe(filters => {
-      setTransactionsList(
-        filterTransactionPerWeek(allTransactions[filters.type], parseInt(filters.week))
-      )
-    })
-    // console.log(filterTransactionPerWeek(allTransactions[filters.type], parseInt(filters.week)))
-  }, [filters?.week])
-
-  // ==> Filter for Type when change allTransactions and type filter
-  useEffect(() => {
-    let isActive = true
-    if (isActive) {
-      subscriptionFilters.subscribe(filters => {
-        allTransactions && setTransactionsList(allTransactions[filters.type])
-      })
-    }
-    setLoad(false)
-    return () => { isActive = false }
-  }, [filters, allTransactions, subscriptionFilters])
-
-  // ==> get Transactions DB when change month filter And setAllTransactions
-  useEffect(() => {
-    let isActive = true
-    if (isActive) {
-      subscriptionFilters.subscribe(filters => {
-        getTransactions(filters.month)
-          .then(transactions => setAllTransactions(transactions))
-      })
-    }
-    setLoad(false)
-    return () => { isActive = false }
-  }, [filters?.month])
+    setTransactionsList(transactionSelector.filteredTransactions)
+  }, [transactionSelector, transactionSelector.filteredTransactions])
 
   return transactionsList
 }
--- src/modules/filters/index.tsx
+++ src/modules/filters/index.tsx
@@ -1,24 +1,28 @@
 import en from "../../public/locale/en"
 import es from "../../public/locale/es"
-import { FilterSelected, Select, Wrapper } from "../../components/filters/styled"
+import { SelectBox, Wrapper } from "../../components/filters/styled"
 import { getWeekOfMonth, getWeeksInMonth } from 'date-fns'
 import { newArrayOfNumbers } from "../../utility/array"
 import { getDaysInMonth } from 'date-fns'
-import { FilterName } from "../../utility/subject-manager"
-import useFilter from "../../components/filters/useFilter"
+import useFilter from "./useFilter"
 import Filter from '../../components/filters/filter'
 import { AnimatePresence } from "framer-motion"
-import Options from "../../components/filters/Options"
+import OptionsList from "../../components/filters/Options"
+import { Options } from "../../components/filters/Options"
+import { getNumberOfMonth } from "../../utility/formatData"
+import SelectedFilter from "./selectedFilter"
 
 type Props = {
   locale: string | undefined
 }
 
+export type FilterDateNames = 'month' | 'week' | 'day'
+
 export interface Filter {
-  idName: FilterName;
+  idName: FilterDateNames;
   name: string
   position: string;
-  dates: string[];
+  dates: Options;
   currentDate: number;
 }
 
@@ -30,26 +34,27 @@ const spring = {
 
 const DataFilters = ({ locale }: Props) => {
   const t = locale === 'en' ? en : es
-  const weeksOfMonth = newArrayOfNumbers(getWeeksInMonth(new Date(), { weekStartsOn: 1 }))
-  const daysOfMonth = newArrayOfNumbers(getDaysInMonth(new Date))
-
+  const weeksOfMonth: Options = newArrayOfNumbers(getWeeksInMonth(new Date(), { weekStartsOn: 1 })).map(day => { return { label: day.toString(), value: day } })
+  const daysOfMonth: Options = newArrayOfNumbers(getDaysInMonth(new Date)).map(day => { return { label: day.toString(), value: day } })
+  const monthOfYear: Options = t.Months.map(month => { return { label: month, value: getNumberOfMonth(month) } })
   const FILTERS: Filter[] = [
     { idName: 'week', name: t.week, position: '1 / 2', dates: weeksOfMonth, currentDate: getWeekOfMonth(new Date(), { weekStartsOn: 1 }) - 1 },
-    { idName: 'month', name: t.month, position: '2/3', dates: t.Months, currentDate: new Date().getMonth() },
+    { idName: 'month', name: t.month, position: '2/3', dates: monthOfYear, currentDate: new Date().getMonth() },
     { idName: 'day', name: t.day, position: '3/4', dates: daysOfMonth, currentDate: new Date().getDate() - 1 }
   ]
   const {
     show,
-    selectedFilter,
     selectedDate,
-    scroll,
+    selectedFilter,
+    scrollRef,
     handleFilterSelected,
-    handleSelectedDate
+    handleSelectedDate,
+    setShow
   } = useFilter({ filter: FILTERS[1] })
 
   return (
     <Wrapper>
-      <Select layout
+      <SelectBox layout
         position={selectedFilter.position}
         transition={spring}
       />
@@ -62,15 +67,18 @@ const DataFilters = ({ locale }: Props) => {
       )}
       <AnimatePresence>
         {show
-          ? <Options
-            scrollRef={scroll}
+          ? <OptionsList
+            scrollRef={scrollRef}
             handleClick={handleSelectedDate}
-            selectedDate={selectedDate}
-            optionsName={selectedFilter.dates}
+            selectedOption={selectedDate}
+            options={selectedFilter.dates}
           />
-          : <FilterSelected>
-            {`${FILTERS[1].dates[FILTERS[1].currentDate]} -- ${selectedFilter.name} ${selectedFilter.currentDate}`}
-          </FilterSelected>
+          : <SelectedFilter
+            handleClick={() => setShow(true)}
+            month={FILTERS[1].dates[FILTERS[1].currentDate].label}
+            nameFilter={selectedFilter.name}
+            selectedDate={selectedDate}
+          />
         }
       </AnimatePresence>
     </Wrapper>
